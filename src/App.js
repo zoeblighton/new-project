0e6759d482a3bf1d2512bf87f3559d16c0c2e016
zoeblighton <zoeblighton.seo@gmail.com>
2025-06-09 20:55:21 +0100

    Added Github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Zoe Blighton and is{" "}
+        <a href="https://github.com/zoeblighton/new-project" target="_blank">
+          {" "}
+          open sourced on Github
+        </a>{" "}
+      </footer>{" "}
+    </div>
+  );
 }
 
 export default App;
